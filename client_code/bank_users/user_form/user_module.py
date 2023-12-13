151f3120f83ea437311b5a08896922a215af2b2e
dinakaryaramati95355@gmail.com <dinakaryaramati95355@gmail.com>
2023-12-13 12:01:42 +0000

    Edited bank_users.user_form, bank_users.user_form.user_module, borrower_registration_form.star_1_borrower_registration_form_begin, borrower_registration_form.star_1_borrower_registration_form_begin_2 and 14 more

--- client_code/bank_users/user_form/user_module.py
+++ client_code/bank_users/user_form/user_module.py
@@ -21,7 +21,7 @@ def find_highest_amigos_id():
     table_data = app_tables.user_profile.search()
     highest_id = 99999
     for row in table_data:
-        coustmer_id = row['coustmer_id']
+        coustmer_id = row['customer_id']
         if coustmer_id > highest_id:
             highest_id = coustmer_id
     return highest_id
@@ -29,7 +29,7 @@ def find_highest_amigos_id():
 # It creates the ID for only new users
 def add_email_and_user_id(email_id):
     generated_id = generate_user_id()
-    app_tables.user_profile.add_row(email_user=email_id, coustmer_id=generated_id,registration_approve=False,profile_status=False,mobile_check=False,last_confirm=False)
+    app_tables.user_profile.add_row(email_user=email_id, customer_id=generated_id,registration_approve=False,profile_status=False,mobile_check=False,last_confirm=False)
 
 #----- end hear ----- #
 
